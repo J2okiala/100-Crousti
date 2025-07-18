4d269dd0df5ca5f309f5fa1d178273f153324502
Joseph <balekialaj@gmail.com>
2025-07-18 18:18:48 +0200

    event

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,24 @@
+import { MouseEvent } from "react";
 
 export default function ListGroup(){
 
     const villes: string[] = ['Monaco', 'Paris', 'Reims', 'Lyon', 'Bordeaux']
+    // villes = []
 
+    const handleClick = (event: MouseEvent) => console.log(event);
+    
     //Affichage
     return(
-        <ul className="list-group">
-            {villes.map((ville,index) => (
-                    <li key={index} className="list-group-item">{ville}</li>
-                ))}
-        </ul>
+        // rendue conditionnel
+        <div>
+            {villes.length === 0 && <p>Aucune ville</p>}
+            <ul className="list-group">
+                {villes.map((ville,index) => (
+                        <li key={index} onClick={handleClick} className="list-group-item">{ville}</li>
+                    ))}
+            </ul>
+
+        </div>
+        
     )
 }
